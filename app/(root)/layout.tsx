8e332c9eaa16190587e9dd07631a0c39236ffd47
Elmahdi -- Elahmer <mehdilhmer4@gmail.com>
2026-01-14 10:08:19 -0800

    Fix dynamic server usage error by forcing dynamic rendering in root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+export const dynamic = "force-dynamic";
 import Image from "next/image";
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
